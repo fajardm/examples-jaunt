5468556bd6c604700fea4057e95606cdc0c7a9d0
fajardm <fajar.dwi.mawan@gmail.com>
2017-05-07 08:52:06 +0700

    Add example 1

---
+++ src/Example1.java
@@ -0,0 +1,18 @@
+/**
+ * Created by fajardm on 01/05/17.
+ */
+
+import com.jaunt.JauntException;
+import com.jaunt.UserAgent;
+
+public class Example1 {
+    public static void main(String[] args) {
+        try {
+            UserAgent userAgent = new UserAgent(); //create new userAgent (headless browser).
+            userAgent.visit("https://news.ycombinator.com/"); //visit a url
+            System.out.println(userAgent.doc.innerHTML()); //print the document as HTML
+        } catch (JauntException e) { //if an HTTP/connection error occurs, handle JauntException.
+            System.err.println(e);
+        }
+    }
+}
